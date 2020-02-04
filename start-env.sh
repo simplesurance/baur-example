45099fc652e6e54849af2e2274056dbb3a7be914
Fabian Holler <fabian.holler@simplesurance.de>
2020-02-04 11:34:28 +0100

    start-env: start a docker registry container
    To be able to upload the artifact of the new myredis app, start-env.sh now also
    starts a docker-registry container that listens on port 5000

--- start-env.sh
+++ start-env.sh
@@ -2,14 +2,18 @@
 
 set -eu -o pipefail
 
-DOCKER_PORT=5433
+PSQL_PORT=5433
+REGISTRY_PORT="5000"
 
 if ! command -v; then
 	echo "docker is not installed"
 	exit 1
 fi
 
-docker run -d -p $DOCKER_PORT:5432 -e POSTGRES_DB=baur postgres:latest
-echo "Started docker PostgreSQL container on port $DOCKER_PORT"
+docker run -d -p $REGISTRY_PORT:5000 --name docker-registry registry:2
+echo "Started docker registry container on port $REGISTRY_PORT"
 
+
+docker run -d -p $PSQL_PORT:5432 -e POSTGRES_DB=baur postgres:latest
+echo "Started docker PostgreSQL container on port $PSQL_PORT"
 echo "Run 'baur init db' to initialize the database for baur"
